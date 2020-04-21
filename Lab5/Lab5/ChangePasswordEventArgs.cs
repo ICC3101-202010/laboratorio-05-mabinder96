73148fcc3053dfa8bb815495eea1241a5a377491
mabinder96 <mbinder@miuandes.cl>
2020-04-21 17:25:02 -0400

    Base

---
+++ Lab5/Lab5/ChangePasswordEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab5
+{
+    public class ChangePasswordEventArgs : EventArgs
+    {
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public string Username { get; set; }
+    }
+}
